a12440961f38cb2aca3f5e5071c9ef70fb0e0b2e
Henzel <henzel.techx@gmail.com>
2025-08-15 11:14:07 +0400

    Done with styles

---
+++ main.js
@@ -0,0 +1,13 @@
+formSubmit()
+
+function formSubmit() {
+    const formContainer = document.getElementById('container');
+    const nameSurname = document.getElementById('name-surname');
+    const address = document.getElementById('address');
+    const phone = document.getElementById('phone');
+    const email = document.getElementById('email');
+    const awareness = document.getElementById('awareness');
+
+    const submitButton = document.getElementById('submit-btn');
+
+}
